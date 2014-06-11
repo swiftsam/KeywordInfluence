52b43830f41660a512e16934a1d95de5886d196c
Sam Swift <samswift@gmail.com>
2014-06-11 16:51:53 -0400

    updates to the capitol words pulling routines so that I don't overwhelm the SF servers

--- pull_capitolwords_text.py
+++ pull_capitolwords_text.py
@@ -1,38 +1,63 @@
-import urllib3
+import urllib3 as url
 import json
 import os.path
 import datetime
 import time
-from multiprocessing import Pool
+import math
 
-# Capitol Words API root for text
-api_root = '/api/1/text.json?'
-http     =  urllib3.connection_from_url('http://capitolwords.org')
+import api_keys as key
+import pull_congresspeople as pullc
 
-def log_error(page):
-  print str(datetime.datetime.now().time()) + ' time out on ' + str(page)
-  with open("data/words/errors.txt", "a") as error_log:
-    error_log.write('page_' + str(page).zfill(6) +',')
+# SunlightFoundation Congress API
+api_text = 'http://capitolwords.org/api/1/text.json?'
 
-def pull_page(page):
+def pull_bioguide_text(bioguide_id):
+  http = url.PoolManager(timeout = 1)
   # check to see if we already have it
-  file_path = 'data/words/page_' + str(page).zfill(6) + '.txt'
+  file_path = 'data/words/by_bioguide/bgid_' + str(bioguide_id) + '.txt'
   if not os.path.isfile(file_path):
-    print str(datetime.datetime.now().time()) + ' getting ' + str(page)
     # query parameters
-    query_params = {'apikey': '05ccd18a42b6443c908952f80fa17c19',
+    query_params = {'apikey': key.sunlight,
                     'sort': 'id asc',
-                    'page': page}
+                    'bioguide_id': bioguide_id}
     # call API
     try:
-      response = http.request('GET', api_root + urllib3.request.urlencode(query_params))
-      results  = json.loads(response.data)['results']
-      if len(results) == 50:
-        with open(file_path, 'w') as outfile:
-          json.dump(results, outfile)
+      response = http.request('GET', api_text + url.request.urlencode(query_params))
+      num_found = json.loads(response.data)['num_found']
+      print str(datetime.datetime.now().time()) + ' getting ' + str(bioguide_id) + ' ['+ str(num_found) +']'
     except:
-      log_error(page)
+      return None
+
+    # check if there were any results
+    if num_found:
+      results  = json.loads(response.data)['results']
+      # check if there were more than 1 page of results, iterate as necessary
+      if num_found > 50:
+        for page in range(1,int(math.floor(num_found/50))+1):
+          print str(datetime.datetime.now().time()) + ' getting ' + str(bioguide_id) + ', page '+ str(page)
+          page_params = query_params
+          page_params['page'] = page
+          try:
+            response = http.request('GET', api_text + url.request.urlencode(page_params))
+            results += json.loads(response.data)['results']
+          except:
+            print "timeout on " + str(bioguide_id) + ', page '+ str(page)
+      # write file with results
+      with open(file_path, 'w') as outfile:
+        json.dump(results, outfile)
+    # if not, write an empty file
+    else:
+      open(file_path, 'a') 
+
+def log_error(bioguide_id):
+  print str(datetime.datetime.now().time()) + ' time out on ' + str(bioguide_id)
+  with open("data/words/errors.txt", "a") as error_log:
+    error_log.write('bgid_' + str(bioguide_id) +',')
 
 
-workers = Pool(15)
-workers.map(pull_page, range(35000))
+for bgid in pullc.pull_congress_ids(538):
+  pull_bioguide_text(bgid)
+
+
+
+
