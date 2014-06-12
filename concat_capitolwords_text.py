3c61141ce86e35828e82c800c159ccd43188380d
Sam Swift <samswift@gmail.com>
2014-06-12 17:56:52 -0400

    finished concatenation of individual congress person text files into a single file

--- concat_capitolwords_text.py
+++ concat_capitolwords_text.py
@@ -2,6 +2,7 @@ import os.path
 from os import listdir
 import json
 import pandas as pd
+import numpy as np
 
 file_path = "data/words/by_bioguide/"
 files     = sorted(list(listdir(file_path)))
@@ -10,12 +11,22 @@ if "errors.txt" in files:
 
 print "loading data from " + str(len(files)) + " files"
 
-data = pd.DataFrame()
-
 for file_name in files:
 	print file_name
 	if os.path.isfile(file_path + file_name) and os.stat(file_path + file_name).st_size:
+		
+		# read JSON file for a single congressperson
 		df = pd.read_json(file_path + file_name)
-		data = pd.concat([data, df])
 
-data.to_csv("data/words.csv")
+		# modify columns
+		df['n_sentence'] = df.speaking.apply(np.size)
+		df['speaking']   = df.speaking.apply(" ".join)
+
+		# concatenate into a single file
+		if file_name == files[0]:
+			df.to_csv("data/words.csv", header=True, mode='w', encoding='utf-8')
+		else:
+			df.to_csv("data/words.csv", header=False, mode='a', encoding='utf-8')
+
+
+
