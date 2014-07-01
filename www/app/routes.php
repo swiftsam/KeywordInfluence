4dfa0ef190b4a73b86d276d76a2b34553a635d25
Sam Swift <samswift@gmail.com>
2014-07-01 12:22:17 -0400

    initialized site using laravel and foundation framework.  test routes working

---
+++ www/app/routes.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
+
+Route::get('/', function()
+{
+	return View::make('pac');
+});
+
+Route::get('pac', function()
+{
+    return View::make('pac');
+});
+
+Route::get('foundation', function()
+{
+    return View::make('foundation');
+});
---
+++ www/app/views/pac.blade.php
@@ -0,0 +1,5 @@
+@extends('layout')
+
+@section('content')
+    PACs
+@stop
