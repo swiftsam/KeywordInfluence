f5a62b2fb8f096c5cec4e8b510e688660acc3768
Sam Swift <samswift@gmail.com>
2014-07-14 11:31:04 -0400

    adding links to opencongress profile of each rep

--- www/app/views/pac.blade.php
+++ www/app/views/pac.blade.php
@@ -5,11 +5,11 @@
 @section('content')
 	<ol class="breadcrumb">
 	  <li><a href='/pac'>PACs</a>  <span class="divider">|</span></li>
-	  <li><small>(Industry)</small> <a href='/industry/{{$pac->prim_code}}'>{{$pac->Industry->catname}}</a>  <span class="divider">|</span></li>
-	  <li class="active">{{ $pac->pac_short }}</li>
+	  <li><small>(Industry)</small> <a href='/industry/{{$pac->prim_code}}'>{{$pac->Industry->catname or ""}}</a>  <span class="divider">|</span></li>
+	  <li class="active">{{ $pac->pac_short or ""}}</li>
 	</ol>
 	
-	<h2>{{ $pac->pac_short }}</h2>
+	<h2>{{ $pac->pac_short or ""}}</h2>
 
 	<!-- Nav tabs -->
 	<ul class="nav nav-tabs" role="tablist" id="pacTabs">
@@ -20,17 +20,7 @@
 	<!-- Tab panes -->
 	<div class="tab-content">
 	  <div class="tab-pane active" id="words">
-	  	Congresspeople who received contributions from {{$pac->pac_short}} used the following phrases more frequently than the average member of congress.
-	  	<ul>
-  		<?php
-			/*$pac_data = json_decode($pac->freqdiff->json_data, true);
-			if(isset($pac_data))
-				foreach($pac_data['ngram'] as $ngram)
-					echo "<li>".$ngram."</li>";
-			else
-				echo "no word use data";*/
-		?>
-		</ul>
+	  	Congresspeople who received contributions from {{$pac->pac_short or ""}} used the following phrases more frequently than the average member of congress.
 		<div id="chart1">
 		  <svg></svg>
 		</div>
@@ -47,7 +37,7 @@
 		        	<td><?php 
 		        		$cp = $contrib->congressperson;
 		        		if(isset($cp)){
-		        			echo("(".$cp->party."-".$cp->state.") ".$cp->firstname." ".$cp->lastname);
+		        			echo("(".$cp->party."-".$cp->state.") <a href='http://www.opencongress.org/people/show/".$cp->govtrack_id."' target='_blank'>".$cp->firstname." ".$cp->lastname."</a>");
 			        	} else {
 			        		echo("<i>not elected</i>");
 			        	}
@@ -65,11 +55,6 @@
   margin: 10px;
   min-width: 100px;
   min-height: 100px;
-/*
-  Minimum height and width is a good idea to prevent negative SVG dimensions...
-  For example width should be =< margin.left + margin.right + 1,
-  of course 1 pixel for the entire chart would not be very useful, BUT should not have errors
-*/
 }
 
 #chart1 svg {
@@ -85,7 +70,7 @@ pac_data = [
 	{   
 		key: 'Series1',
 	    color: '#d62728',
-	    values: {{ $pac->freqdiff->json_data }}
+	    values: {{ $pac->freqdiff->json_data or "" }}
 	}
 ];
 
@@ -98,7 +83,7 @@ pac_data = [
 		nv.addGraph(function() {
 		  chart = nv.models.multiBarHorizontalChart()
 		      .x(function(d) { return d.ngram })
-		      .y(function(d) { return d.freq_diff })
+		      .y(function(d) { return d.freq_ratio })
 		      .margin({top: 30, right: 20, bottom: 50, left: 175})
 		      .showValues(true)
 		      .tooltips(false)
@@ -108,11 +93,10 @@ pac_data = [
 		      .showControls(false);
 
 		  chart.yAxis
-		      .tickFormat(d3.format(',.4f'));
+		      .tickFormat(d3.format(',.1f'));
 
 		  d3.select('#chart1 svg')
 		      .datum(pac_data)
-		      //.datum(long_short_data)
 		      .call(chart);
 
 		  nv.utils.windowResize(chart.update);
